efe095c4b250438e0fd7c29709c486bdfd977de0
Nijamuddin Ansari <76437265+nizxcoder@users.noreply.github.com>
2024-02-26 16:50:41 +0530

    apex chart fixed

--- src/Components/Small Components/AreaChart.js
+++ src/Components/Small Components/AreaChart.js
@@ -1,43 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 
 function AreaChart(props) {
-    const [arr,setArr] = useState([])
-    useEffect(()=>{
-        console.log(props.chartarr);
-        setArr(props.chartarr)
-    },[arr])
-    return (
-        <>
-            <div className="container-fluid">
-                <Chart 
-                type='area'
-                width={900}
-                height={150}
-                series={[
-                    {
-                        name:'Forecast',
-                        data:arr
-                    }
-                ]}
-                options={{
-                    title:{
-                        style:{fontSize:20}
-                    },
-                    colors:['#3083dc'],
-                    stroke:{width:0,curve:'smooth'},
-                    fill:{opacity:1,type:'solid'},
-                    dataLabels:{
-                        enabled:false
-                    }
-                }
-            }
-                >
+  const [arr, setArr] = useState(props.chartarr);
 
-                </Chart>
-            </div>
-        </>
-    )
+  useEffect(() => {
+    console.log(props.chartarr);
+    setArr(props.chartarr);
+  }, [props.chartarr]);
+  return (
+    <>
+      <div className="container-fluid">
+        <Chart
+          type="area"
+          width={'103%'}
+          height={180}
+          series={[
+            {
+              name: 'Forecast',
+              data: arr,
+            },
+          ]}
+          options={{
+            chart: {
+              id: 'forecast-temp',
+              toolbar: {
+                show: false,
+              },
+            },
+            title: {
+              style: { fontSize: 20 },
+            },
+            colors: ['#3083dc'],
+            stroke: { width: 0, curve: 'smooth' },
+            fill: { opacity: 1, type: 'solid' },
+            dataLabels: {
+              enabled: false,
+            },
+            yaxis: {
+              stepSize: 1,
+            },
+          }}
+        ></Chart>
+      </div>
+    </>
+  );
 }
 
-export default AreaChart
+export default AreaChart;

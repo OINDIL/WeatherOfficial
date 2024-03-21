295a832285f72ac320a613ff3dc7d658d5a8ee68
oindil <oindilgolder303@gmail.com>
2024-03-21 16:21:35 +0530

    added enter key search feature

--- Client/src/Components/Small Components/SearchBar.js
+++ Client/src/Components/Small Components/SearchBar.js
@@ -3,7 +3,11 @@ import './CSS/SearchBar.css'
 function SearchBar({getData}) {
 
     const [inputValue,setInputValue] = useState(null)
-
+    const handleEnterKey = (event) =>{
+        if(event.key === 'Enter'){
+            getData(inputValue)
+        }
+    }
     return (
         <>
         <div className="container" id='search-container'>
@@ -11,6 +15,7 @@ function SearchBar({getData}) {
                 <input type="text" className="form-control" placeholder="Add a Location" aria-describedby="button-addon2"
                 onChange={(e)=>setInputValue(e.target.value)}
                 autoFocus
+                onKeyDown={handleEnterKey}
                 />
                     <button className="btn btn-primary" type="button" id="button-addon2" onClick={()=>getData(inputValue)}>Add</button>
             </div>

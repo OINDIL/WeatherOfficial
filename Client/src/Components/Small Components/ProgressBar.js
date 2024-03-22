f8b60a2309f44aae9a15a07dfd5daf95cab8862f
oindil <oindilgolder303@gmail.com>
2024-03-22 14:15:00 +0530

    progress bar data is now changing accoirging to toggle

--- Client/src/Components/Small Components/ProgressBar.js
+++ Client/src/Components/Small Components/ProgressBar.js
@@ -4,7 +4,7 @@ function ProgressBar({ data,data_f,active }) {
     return (
         <div>
             <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                <div className="progress-bar" style={{width: active ? `${data_f}%` : `${data}%`}}></div>
+                <div className="progress-bar bg-primary" style={{width: active ? `${data_f}%` : `${data}%`}}></div>
             </div>
         </div>
     )
